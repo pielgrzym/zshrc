2a4774df8edd5faf7096d58efcb0a647bf13891c
Jakub Nawalaniec <jakub.nawalaniec@getbase.com>
2013-08-18 00:36:56 +0200

    A more civilised printf ;)

--- zsh.d/marks.zsh
+++ zsh.d/marks.zsh
@@ -16,7 +16,10 @@ unmark() {
 
 marks() {
         for link in $MARKPATH/*(@); do
-                printf "\t$fg[cyan]${link:t}$reset_color\t -> $fg[blue]$(readlink $link)$reset_color\t\n"
+                local markname="$fg[cyan]${link:t}$reset_color"
+                local markpath="$fg[blue]$(readlink $link)$reset_color"
+                printf "%s\t" $markname
+                printf "-> %s \t\n" $markpath
         done
 }
 alias ms=marks
